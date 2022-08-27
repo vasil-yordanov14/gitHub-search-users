870e0d5bf6a6daf5896807cd4b7231880f77db0b
vasil-yordanov14 <vasil_sh@abv.bg>
2022-08-27 15:30:24 +0300

    repos and followers

--- src/context/context.js
+++ src/context/context.js
@@ -26,7 +26,20 @@ const GithubProvider = ({ children }) => {
     console.log(response);
     if (response) {
       setGithubUser(response.data);
+      const { login, followers_url } = response.data;
+      // repos
+      axios(`${rootUrl}/users/${login}/repos?per_page=100`).then((response) =>
+        setRepos(response.data)
+      );
+      // followers
+      axios(`${followers_url}?per_page=100`).then((response) =>
+        setFollowers(response.data)
+      );
       // more logic here
+      // repos
+      // https://api.github.com/users/john-smilga/repos?per_page=100
+      // followers
+      // https://api.github.com/users/john-smilga/followers
     } else {
       toggleError(true, `there is no user with that username`);
     }
